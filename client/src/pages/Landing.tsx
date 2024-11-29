ea21d00b7d6a1ed90420caa8c9f89e8c11d9e286
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2024-11-29 18:01:15 -0500

    fixed env example and other fixes

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -12,8 +12,8 @@ function Landing() {
       <Row>
         <Col className="landing-hero-image" xs="12" sm="6"></Col>
         <Col className="d-flex flex-column justify-content-center mt-md-4" xs="12" md="6">
-          <h1 className="text-center">FirstStep_Fitness</h1>
-          <h3 className="text-center fw-light">Embrace the burn, for every drop of sweat is a step closer to strength, resilience, and the best version of yourself!</h3>
+          <h1 className="text-center">Elevate Your Fitness Journey</h1>
+          {/* <h3 className="text-center fw-light">Embrace the burn, for every drop of sweat is a step closer to strength, resilience, and the best version of yourself!</h3> */}
         </Col>
       </Row>
       <Container>
